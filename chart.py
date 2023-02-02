eb64e7cb19e33c2158da3b7c22dc3a1d58a21feb
DavinaLiang <jiawl9797@outlook.com>
2023-02-02 16:43:24 +0800

    2nd update

--- chart.py
+++ chart.py
@@ -18,7 +18,7 @@ st.set_page_config(layout="wide",page_icon="💰",page_title="How Finance Works"
 #add a title
 st.title('Financial Analysis')
 space(1)
-st.image('photo1.jpeg')
+#st.image('photo1.jpeg')
 st.markdown('##### These 2 charts are used to assess the operating condition of the chosen company ')
 space(2)
 ###############data preparation
@@ -29,13 +29,10 @@ Companies = ['Jiaotong','Xiaomi','Yongan']
 #effortless caching: relieve long-running computation in your code for continuously updating
 @st.cache
 def load_fdata(data):
-    data1 = pd.read_excel(open(data, 'rb'),
-              sheet_name='Sheet1',index_col='Date')
-    data1[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    data2 = pd.read_excel(open(data, 'rb'),
-              sheet_name='Sheet2',index_col='Date')
-    data2[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data1,data2
+    data = pd.read_csv(data)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    data.set_index("Date",inplace=True)
+    return data
 
 def Metrics_Calc(data):
   data['Days Sales Out.']=(data['A/R']/data['Revenue'])*365
@@ -47,7 +44,7 @@ space(2)
 
 ############## Visualze the data
 def Chart(data,title):
-  graph = sns.relplot(data=data,kind='line',height=6, aspect=11.7/8.27,linewidth=3)
+  st.line_chart(data)
   plt.title(title,fontsize='xx-large',fontweight='heavy')
   plt.xticks(rotation=45, ha='right')
   plt.axhline(0, ls='--', linewidth=2, color='red')
@@ -55,25 +52,38 @@ def Chart(data,title):
 def Visual_Metrics(data):
   visual_metrics = data[['Days Sales Out.','Days Inventory Out.','Days Payable Out.','Cash Conversion Cycle']]
   return visual_metrics
-##########Visualizing stock price
+
+##########Visualizing stock price1
 #Load data from csv files
-st.header('Economic Returns')
-space(1)
 
-mydir = "data"
-xlsxfiles = glob.glob(os.path.join(mydir, '*.xlsx'))
-df_dict = dict()
-for file in xlsxfiles:
-    df_dict[file.split('/')[2].split('.')[0]] = load_fdata(file)
 
-s1, s2 = st.columns(2)
+mydir1 = "data/chart1"
+mydir2 = "data/chart2"
+
+csvfiles1 = glob.glob(os.path.join(mydir1, '*.csv'))
+df_dict1 = dict()
+for file in csvfiles1:
+    df_dict1[file.split('/')[2].split('.')[0]] = load_fdata(file)
+
+csvfiles2 = glob.glob(os.path.join(mydir2, '*.csv'))
+df_dict2 = dict()
+for file in csvfiles2:
+    df_dict2[file.split('/')[2].split('.')[0]] = load_fdata(file)
+st.write(df_dict2)
+
 option = st.selectbox(
      'Choose one company to visualize',
      Companies)
-data = df_dict[option]
-Metrics_Calc(data)
+data1 = df_dict1[option]
+data2 = df_dict2[option]
+Metrics_Calc(data2)
+s1, s2 = st.columns(2)
 with s1:
-    Chart(data,option)
+    st.header('Economic Returns')
+    space(1)
+    Chart(data1,option)
 
 with s2:
-    Chart(Visual_Metrics(data),option)
+    st.header('Conversion Cycle')
+    space(1)
+    Chart(Visual_Metrics(data2),option)
