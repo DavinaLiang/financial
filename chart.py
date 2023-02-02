afd7f2955adf171907e4cfc3fc132417fbe72d73
DavinaLiang <jiawl9797@outlook.com>
2023-02-02 17:07:21 +0800

    5

--- chart.py
+++ chart.py
@@ -27,7 +27,7 @@ Companies = ['Xiaomi','Viva BioTech']
 
 
 #effortless caching: relieve long-running computation in your code for continuously updating
-@st.cache(ignore_hash=True)
+@st.cache(allow_output_mutation=True)
 def load_fdata(data):
     data = pd.read_csv(data)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
