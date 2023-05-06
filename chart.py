e7f7e098acf93ff294ffb229ed3ebac47d3064e3
DavinaLiang <jiawl9797@outlook.com>
2023-05-06 17:37:09 +0800

    ooo

--- chart.py
+++ chart.py
@@ -41,7 +41,7 @@ ticker = {
 
 
 #effortless caching: relieve long-running computation in your code for continuously updating
-@st.cache_data
+@st.cache_resource
 def load_fdata(data):
     data = pd.read_csv(data)
     data.index = pd.to_datetime(data['Date'], format='%Y-%m-%d').dt.year
